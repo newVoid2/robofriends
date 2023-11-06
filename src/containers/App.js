96b8d0f1f1120a118d405ce60e2b9aeb67fdf5cd
Richard <rkerr022@gmail.com>
2023-11-06 18:10:46 -0500

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+import { requestRobots, setSearchField } from '../actions';
 
-function App() {
-	const [robots, setRobots] = useState([]);
-	const [searchField, setSearchField] = useState('');
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+function App(props) {
 	const [count, setCount] = useState(0);
+	const {searchField, onSearchChange, onRequestRobots, robots, isPending} = props;
 
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		    .then(response => response.json())
-		    .then(users => {setRobots(users)});
-		    console.log(count);
+		onRequestRobots();
+		console.log(count);
 	}, [count]) // only run if count changes.
-
-	const onSearchChange = (event) => {
-		setSearchField(event.target.value);
-		
-	}
 
 	const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 	});
-	return !robots.length ? 
+	return isPending ? 
 		<h1>Loading</h1> :
 	    (
 		<div className='tc'>
@@ -41,4 +51,4 @@ function App() {
 	    );  
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
